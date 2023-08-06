a57ccb3e77f25aed9074b9a8452f7e6e80cb3ed3
Munish Rana <sam786rana786@gmail.com>
2023-08-06 22:29:11 +0530

    first commit

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        $user = User::create($request->getData());
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('kluhkjhkjhkhkbjnbctfdszt')->plainTextToken
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Task;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskApiResource;
+
+class TaskController extends Controller
+{
+
+    // public function __construct()
+    // {
+    //     $this->authorizeResource(Task::class);
+    // }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TaskApiResource::collection(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+
+        return TaskApiResource::make($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return TaskApiResource::make($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+
+        return TaskApiResource::make($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+}

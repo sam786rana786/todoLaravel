a0c64d107e48c59e48930c5d4ccd777c32a7ed88
Munish Rana <sam786rana786@gmail.com>
2023-08-07 15:05:33 +0530

    Disabled apiv1 path

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-require __DIR__ . '/api/v1.php';
+// require __DIR__ . '/api/v1.php';
 require __DIR__ . '/api/v2.php';
 
 Route::prefix('auth')->group(function() {
